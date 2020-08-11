6173d3869c71a3f57281d938f898e8153937a792
zequnyu9503 <greatyuzequn@outlook.com>
2020-08-11 09:54:48 +0800

    compile spark.

--- spark-update-all.sh
+++ spark-update-all.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 slaves=("node7" "node8" "node9" "node10" "node11")
+root="/opt/zequnyu/sh"
 
 SPARK_HOME="/opt/service/spark/spark-2.4.4"
 
 if [[ $(hostname) = "node6" ]];then
-sh halt.sh
+sh ${root}/spark-halt.sh
 for slave in ${slaves[@]}
  do
   ssh "root@${slave}" "cd ${SPARK_HOME} && git pull && mvn clean install -DskipTests -pl core,assembly,examples" &
  done
 wait
-sh start.sh
+sh ${root}/spark-launch.sh
 fi
 
