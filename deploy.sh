34cc8a9f1f46f6244ecf9f8854900cca905c4a7d
zequnyu9503 <greatyuzequn@outlook.com>
2020-08-11 10:02:37 +0800

    update deploy.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 master="node6"
 slaves=("node7" "node8" "node9" "node10" "node11" )
 
+ssh root@${master} "chmod 777 /opt/service/spark/spark-2.4.4/sbin/* && chmod 777 /opt/service/spark/spark-2.4.4/bin/*"
+
 for slave in ${slaves[@]}
 do
     # ssh root@${slave} "cd /opt && mkdir service && mkdir zequnyu && cd service && mkdir local && mkdir spark"
@@ -12,8 +14,9 @@ do
     # scp -r "/opt/service/scala-2.11.12" "root@${slave}:/opt/service/"
     # scp -r "/opt/service/spark/spark-2.4.4" "root@${slave}:/opt/service/spark/"
     # scp -r "/opt/service/spark" "root@${slave}:/opt/service/"
-    ssh root@${slave} "cd /opt/service/ && rm -rf spark && mkdir -p spark/spark-local"
-    scp -r "/opt/service/spark/spark-2.4.4" "root@${slave}:/opt/service/spark"
+    # ssh root@${slave} "cd /opt/service/ && rm -rf spark && mkdir -p spark/spark-local"
+    # scp -r "/opt/service/spark/spark-2.4.4" "root@${slave}:/opt/service/spark"
+    ssh root@${slave} "chmod 777 /opt/service/spark/spark-2.4.4/sbin/* && chmod 777 /opt/service/spark/spark-2.4.4/bin/*"
 
     # scp /root/.bashrc "root@${slave}:/root/"
     # ssh root@${slave} "source /root/.bashrc"
