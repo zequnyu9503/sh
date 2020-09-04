cd3ff3329fe1db7d0c432795026eebc76422e34d
zequnyu9503 <greatyuzequn@outlook.com>
2020-09-04 20:39:03 +0800

    update

--- sliding-sliding-update.sh
+++ sliding-sliding-update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd "/opt/service/spark/slidingwindow" &&  mvn clean install -DskipTests -pl sliding
+cd "/opt/service/spark/slidingwindow" &&  git pull && mvn clean install -DskipTests -pl sliding
