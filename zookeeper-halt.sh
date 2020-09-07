5aa7d10048915af910cfb419a179d43fa2af6157
zequnyu9503 <greatyuzequn@outlook.com>
2020-09-07 18:46:00 +0800

    fix configurations.

---
+++ zookeeper-halt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+slaves=("node6" "node7" "node8" "node9" "node10" "node11" )
+path="/opt/service/zookeeper-3.5.8/bin/"
+
+for slave in ${slaves[@]}
+do
+ssh root@${slave} "cd ${path} && sh zkServer.sh stop"
+done
