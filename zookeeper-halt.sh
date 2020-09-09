28d25db809f9a1047b79d4bedf985c80179e3031
zequnyu9503 <greatyuzequn@outlook.com>
2020-09-09 16:07:33 +0800

    fix configurations.

--- zookeeper-halt.sh
+++ zookeeper-halt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 slaves=("node6" "node7" "node8" "node9" "node10" "node11" )
-path="/opt/service/zookeeper-3.5.8/bin/"
+path="/opt/service/zookeeper-3.4.14/bin/"
 
 for slave in ${slaves[@]}
 do
