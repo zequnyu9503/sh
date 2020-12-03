f188f8c5dbaa50f0c83f73bf0fa4197834c6d579
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-03 23:37:22 +0800

    更新Sliding 编译Core和Assembly

--- sliding-spark-update.sh
+++ sliding-spark-update.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-slaves=("node6" "node7 node8" )
-
 path="/opt/service/spark/slidingwindow"
-
-for slave in ${slaves[@]}
-do
-    ssh root@${slave} "cd ${path} && git pull && mvn clean install -DskipTests -pl core,sliding,assembly" &
-done
+cd ${path} && git pull && mvn clean install -DskipTests -pl core, assembly
