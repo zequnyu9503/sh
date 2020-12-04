51d9cc12ae08e601ab4da138c3d50743f486f0c6
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-04 15:36:57 +0800

    更新SH脚本

---
+++ sliding-sliding.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+path="/opt/service/spark/slidingwindow"
+slaves=("node6 node9 node10 node11")
+
+echo "更新node6 node9 node10 node11"
+echo "只更新Sliding"
+
+for slave in ${slaves[@]}
+ do
+  ssh root@${slave} "cd ${path};git pull;mvn clean install -DskipTests -pl sliding" &
+ done
+wait
+echo "更新完毕"
--- sliding-start.sh
+++ sliding-start.sh
@@ -5,9 +5,9 @@ MASTER_URL="spark://node6:7079"
 SPARK_HOME="/opt/service/spark/slidingwindow"
 
 if [[ $(hostname) = "node6" ]];then
-echo "Launch sliding on Master node."
+echo "启动Master"
 sh "${SPARK_HOME}/sbin/start-master.sh"
-
+echo "启动Workers"
 for slave in ${slaves[@]}
  do
   ssh "root@${slave}" sh "${SPARK_HOME}/sbin/start-slave.sh" -h ${slave} ${MASTER_URL}
