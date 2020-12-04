554b36122850ebe8d56e0a3ca01d1069c9545526
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-04 17:06:59 +0800

    sliding

---
+++ run-sliding-function.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+libs_dir="hdfs://node6:9000/libs"
+log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
+target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
+parameters="hdfs://node6:9000/real-world/trips2019.csv 10"
+
+spark-submit \
+--master spark://node6:7079 \
+--executor-memory 32g \
+--executor-cores 32 \
+--driver-cores 16 \
+--driver-memory 16g \
+--class pers.yzq.sliding.debug.Function \
+--driver-java-options "-Dlog4j.configuration=file:${log_path}" \
+${target} ${parameters}
