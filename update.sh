f0097255cb34445d5da575b9238e48fe95ca74ca
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-04 15:09:19 +0800

    更新SH脚本

--- update.sh
+++ update.sh
@@ -2,4 +2,11 @@
 # mvn dependency:copy-dependencies -DoutputDirectory=./libs
 cur_dir=$(pwd)
 sh_dir="/opt/zequn/sh"
-cd ${sh_dir} && git pull && cd ${cur_dir}
+slaves=("node6 node9 node10 node11")
+
+echo "更新node6 node9 node10 node11"
+for slave in ${slaves[@]}
+ do
+  ssh "root@${slave}" cd ${sh_dir} && git pull
+ done
+echo "更新完毕"
