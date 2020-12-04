b2b1866c11130f03633465c4cee1473ccb01b385
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-04 15:10:29 +0800

    更新SH脚本

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # mvn dependency:copy-dependencies -DoutputDirectory=./libs
 cur_dir=$(pwd)
-sh_dir="/opt/zequn/sh"
+sh_dir="/opt/zequnyu/sh"
 slaves=("node6 node9 node10 node11")
 
 echo "更新node6 node9 node10 node11"
