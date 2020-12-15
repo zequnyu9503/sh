f5fc3c1d93b7ce2bb9f8ea36af6e407ef0d630c6
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-15 20:22:07 +0800

    更新sliding脚本

---
+++ run-sliding-tweet.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+libs_dir="hdfs://node6:9000/libs"
+log_path="/opt/service/spark/spark-2.4.4/conf/log4j.properties"
+target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
+parameters="hdfs://node6:9000/real-world/2019-04-01.json"
+
+spark-submit \
+--master spark://node6:7079 \
+--executor-memory 32g \
+--executor-cores 16 \
+--driver-cores 16 \
+--driver-memory 16g \
+--class pers.yzq.sliding.dataset.Tweet \
+--driver-java-options "-Dlog4j.configuration=file:${log_path}" \
+--jars \
+${libs_dir}/fastjson-1.2.35.jar \
+${target} ${parameters}
