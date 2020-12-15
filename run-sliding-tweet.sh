d34715dd6146ef36e5620bf4f606e68ce3d96c2e
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-15 20:35:56 +0800

    更新sliding脚本

--- run-sliding-tweet.sh
+++ run-sliding-tweet.sh
@@ -2,9 +2,9 @@
 libs_dir="hdfs://node6:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
-parameters="hdfs://node6:9000/real-world/2019-04-01.json"
+parameters=("2019-04-01" "2019-04-02" "2019-04-03" "2019-04-04" "2019-04-05")
 
-spark-submit \
+alias run=spark-submit \
 --master spark://node6:7079 \
 --executor-memory 32g \
 --executor-cores 16 \
@@ -14,4 +14,9 @@ spark-submit \
 --driver-java-options "-Dlog4j.configuration=file:${log_path}" \
 --jars \
 ${libs_dir}/fastjson-1.2.35.jar \
-${target} ${parameters}
+${target}
+
+for file in ${parameters[@]}
+do
+  run "hdfs://node6:9000/real-world/${file}.json"
+done
