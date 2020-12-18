0750fc76d94abe8fb7c5a946c8b04cc2f59ec25e
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-18 23:03:55 +0800

    Hbase脚本

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ do
     # ssh root@${slave} "cd /opt/service/ && rm -rf spark && mkdir -p spark/spark-local"
     # scp -r "/opt/service/spark/spark-2.4.4" "root@${slave}:/opt/service/spark"
     # ssh root@${slave} "chmod 777 /opt/service/spark/spark-2.4.4/sbin/* && chmod 777 /opt/service/spark/spark-2.4.4/bin/*"
-     # scp /root/.bashrc "root@${slave}:/root/"
-     # ssh root@${slave} "source /root/.bashrc"
+     scp /root/.bashrc "root@${slave}:/root/"
+     ssh root@${slave} "source /root/.bashrc"
       # scp /opt/service/hbase-2.1.4/conf/hbase-site.xml root@${slave}:/opt/service/hbase-2.1.4/conf/
       # ssh root@${slave} rm -rf /opt/service/hbase-2.1.4
-      scp -r /opt/service/hbase-2.1.4 root@${slave}:/opt/service/
+      # scp -r /opt/service/hbase-2.1.4 root@${slave}:/opt/service/
 done
