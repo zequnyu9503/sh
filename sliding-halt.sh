5eea48ad7371bc2584aaf2b4ef9eafe005dc8eed
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-22 17:33:48 +0800

    spark

--- sliding-halt.sh
+++ sliding-halt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SPARK_HOME="/opt/service/spark/slidingwindow"
 
-if [[ $(hostname) -eq "node6" ]];then
+if [[ $(hostname) -eq "node1" ]];then
 echo "Spark comes to a halt on Master node."
 sh ${SPARK_HOME}/sbin/stop-master.sh
 sh ${SPARK_HOME}/sbin/stop-slaves.sh
--- sliding-start.sh
+++ sliding-start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-slaves=("node9 node10 node11")
+slaves=("node7 node8 node9 node10 node11")
 
-MASTER_URL="spark://node6:7079"
+MASTER_URL="spark://node1:7079"
 SPARK_HOME="/opt/service/spark/slidingwindow"
 
-if [[ $(hostname) = "node6" ]];then
+if [[ $(hostname) = "node1" ]];then
 echo "启动Master"
 sh "${SPARK_HOME}/sbin/start-master.sh"
 echo "启动Workers"
