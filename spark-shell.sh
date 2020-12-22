bc9c63c5509374935f873ec7d5bd0245decb98ec
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-22 19:14:13 +0800

    spark

--- spark-shell.sh
+++ spark-shell.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-libs_dir="hdfs://node6:9000/libs"
+libs_dir="hdfs://node1:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 
 spark-shell \
---master spark://node6:7079 \
+--master spark://node1:7079 \
 --executor-memory 32g \
 --executor-cores 32 \
 --driver-cores 16 \
