dd7ff5032da1f48f7065c1f5904138bca8062999
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-22 10:17:32 +0800

    hbase

--- grep-env-all.sh
+++ grep-env-all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 slaves=("node1" "node7" "node8" "node9" "node10" "node11" "node12" "node13" "node14" "node15")
+param=$0
 
 for slave in ${slaves[@]}
  do
-  echo -e "${slave} >>\n $(ssh root@${slave} env | grep $0)"
+  echo -e "${slave} >>\n $(ssh root@${slave} env | grep ${param})"
  done
