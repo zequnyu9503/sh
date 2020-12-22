5e46a73d7dd8514bf5a2fa5a1a5502cc8704eb28
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-22 11:22:49 +0800

    hbase

--- hbase/update-conf.sh
+++ hbase/update-conf.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-path="/opt/service/hbase-2.3.3/conf/"
+path="/opt/service/hbase-2.3.3/"
 slaves=("node7" "node8" "node9" "node10" "node11" "node12" "node13" "node14" "node15")
 
 for slave in ${slaves[@]}
  do
-  scp "${path}*" root@${slave}:${path}
+  scp -r "${path}conf" root@${slave}:${path}
  done
 echo "更新完毕"
