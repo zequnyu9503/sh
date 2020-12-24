d4d677ae5b03a0931ee6beb591a2afd6bbcf8803
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-24 15:41:26 +0800

    spark

---
+++ hbase/hbase-halt.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+HBASE_HOME="/opt/service/hbase-2.1.4/bin/stop-hbase.sh"
+
+sh ${HBASE_HOME}
--- zookeeper/zookeeper-halt.sh
+++ zookeeper/zookeeper-halt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 slaves=("node1" "node7" "node8" "node9" "node10" "node11" "node12" "node13" "node14" "node15" )
-path="/opt/service/zookeeper-3.4.14/bin/"
+path="/opt/service/zookeeper-3.5.5/bin/"
 
 for slave in ${slaves[@]}
 do
-ssh root@${slave} "cd ${path} && sh zkServer.sh stop && rm -rf /opt/service/zoodir/version-2"
+ssh root@${slave} "cd ${path} && sh zkServer.sh stop"
 done
