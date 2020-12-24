4afadf2538006ecd2b96f0130966c06174dd0421
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-24 16:39:26 +0800

    spark

--- hadoop/check-env.sh
+++ hadoop/check-env.sh
@@ -8,9 +8,12 @@ slaves=("node1" "node7" "node8" "node9" "node10" "node11" "node12" "node13" "nod
 
 for slave in ${slaves[@]}
 do
- ssh root@${slave} "if [ ! -d ${ROOT}/namenode ]; then mkdir -p ${ROOT}/namenode; else rm -rf ${ROOT}/namenode fi"
- ssh root@${slave} "if [ ! -d ${ROOT}/datanode ]; then mkdir -p ${ROOT}/datanode; else rm -rf ${ROOT}/datanode fi"
- ssh root@${slave} "if [ ! -d ${ROOT}/hadoop-pid-dir ]; then mkdir -p ${ROOT}/hadoop-pid-dir; else rm -rf ${ROOT}/hadoop-pid-dir fi"
+  if ssh root@${slave} '[ -d ${ROOT}/namenode ]'; then
+    echo "a"
+  fi
+ #ssh root@${slave} "if [ ! -d ${ROOT}/namenode ]; then mkdir -p ${ROOT}/namenode; else rm -rf ${ROOT}/namenode fi"
+ #ssh root@${slave} "if [ ! -d ${ROOT}/datanode ]; then mkdir -p ${ROOT}/datanode; else rm -rf ${ROOT}/datanode fi"
+ #ssh root@${slave} "if [ ! -d ${ROOT}/hadoop-pid-dir ]; then mkdir -p ${ROOT}/hadoop-pid-dir; else rm -rf ${ROOT}/hadoop-pid-dir fi"
  ssh root@${slave} "echo $(env | grep HADOOP)"
 done
 echo "检查完毕"
