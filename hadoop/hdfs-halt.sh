66dd5e2b427d1166c6de13861db8f930a4c167bd
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-24 15:44:23 +0800

    spark

--- hadoop/hdfs-halt.sh
+++ hadoop/hdfs-halt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-path="/opt/service/hadoop-2.8.5/sbin/"
+path="/opt/service/hadoop-2.7.7/sbin/"
 
 echo "主节点是node1"
 echo "从节点是node7,node8,node9,node10,node11,node12,node13,node14,node15"
