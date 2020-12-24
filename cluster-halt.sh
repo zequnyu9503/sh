3acbc58af967e34876c7832d28e1267e6da5f9ab
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-24 18:01:51 +0800

    spark

--- cluster-halt.sh
+++ cluster-halt.sh
@@ -3,5 +3,5 @@
 echo "依次关闭HBase,HDFS, 不关闭Zookeeper."
 echo "主节点是node1"
 echo "从节点是node7,node8,node9,node10,node11,node12,node13,node14,node15"
-sh hbase/hbase-halt.sh
-sh hadoop/hdfs-halt.sh
+sh /opt/zequnyu/sh/hbase/hbase-halt.sh
+sh /opt/zequnyu/sh/hadoop/hdfs-halt.sh
--- cluster-start.sh
+++ cluster-start.sh
@@ -4,5 +4,5 @@ echo "依次启动HDFS,HBase, 不启动Zookeeper."
 echo "主节点是node1"
 echo "从节点是node7,node8,node9,node10,node11,node12,node13,node14,node15"
 
-sh hadoop/hdfs-start.sh
-sh hbase/hbase-start.sh
+sh /opt/zequnyu/sh/hadoop/hdfs-start.sh
+sh /opt/zequnyu/sh/hbase/hbase-start.sh
