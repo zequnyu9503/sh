7d423b33505ae37aa4790036704a32d813edd6bf
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-25 19:13:02 +0800

    修改资源配置.

--- sliding-update.sh
+++ sliding-update.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 path="/opt/service/spark/slidingwindow"
-slaves=("node1 node7 node8 node9 node10 node11 node13 node14 node15")
+slaves=("node1 node7 node8 node9 node10 node11")
 
-echo "node1 node7 node8 node9 node10 node11 node13 node14 node15"
+echo "node1 node7 node8 node9 node10 node11"
 echo "只更新Core, Assembly"
 
 for slave in ${slaves[@]}
  do
-  ssh root@${slave} "cd ${path};git reset --hard;git pull;mvn clean install -DskipTests -pl core,assembly" &
+  ssh root@${slave} "cd ${path};git pull;mvn clean install -DskipTests -pl core,assembly" &
  done
 wait
 echo "更新完毕"
