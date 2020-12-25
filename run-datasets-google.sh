4fb5fc91330b08113271e1ed2f0e9cef98b89160
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-25 18:18:04 +0800

    修改资源配置.

--- run-datasets-google.sh
+++ run-datasets-google.sh
@@ -6,8 +6,8 @@ hdfs dfs -rm -r /google/new_task_events
 
 spark-submit \
 --master spark://node1:7079 \
---executor-memory 24g \
---executor-cores 16 \
---driver-cores 16 \
+--executor-memory 32g \
+--executor-cores 24 \
+--driver-cores 8 \
 --class ${className} \
 /opt/service/spark/spark-app/target/spark-2.0.jar
