4fdea59d5dadda717e59542a12f3b68e52fb1d7e
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-25 11:29:20 +0800

    spark

--- sliding-update.sh
+++ sliding-update.sh
@@ -7,7 +7,7 @@ echo "只更新Core, Assembly"
 
 for slave in ${slaves[@]}
  do
-  ssh root@${slave} "cd ${path};git pull;mvn clean install -DskipTests -pl core,assembly" &
+  ssh root@${slave} "cd ${path};git reset --hard;git pull;mvn clean install -DskipTests -pl core,assembly" &
  done
 wait
 echo "更新完毕"
