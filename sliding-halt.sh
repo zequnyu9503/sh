9a2633ca6f001e59a5916828250d61aaef015a6d
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-26 11:36:22 +0800

    修改资源配置.

--- sliding-halt.sh
+++ sliding-halt.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-slaves=("node7 node8 node9 node10 node11 node13 node14 node15")
+slaves=("node7 node8 node9 node10 node11 node12 node13 node14 node15")
 
 MASTER_URL="spark://node1:7079"
 SPARK_HOME="/opt/service/spark/slidingwindow"
 echo "关闭Workers"
 for slave in ${slaves[@]}
  do
+  printf "关闭 ${slave} 节点: "
   ssh "root@${slave}" sh "${SPARK_HOME}/sbin/stop-slave.sh"
  done
 
