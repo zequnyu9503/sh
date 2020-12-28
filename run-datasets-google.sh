4f7ab58f9cce8e8f066f6e1d856bb5bbf3f7ed05
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-28 12:55:50 +0800

    修改资源配置.

--- run-datasets-google.sh
+++ run-datasets-google.sh
@@ -2,6 +2,9 @@
 # 类名.
 className=pers.yzq.spark.datasets.Google
 
+# hdfs dfs -rm -r /google/res_1
+hdfs dfs -rm -r /google/new_task_usage
+
 spark-submit \
 --master spark://node1:7079 \
 --executor-memory 32g \
