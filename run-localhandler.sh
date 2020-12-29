4090a4fb5026d5d50b76c4194a77be467bcaf82e
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-29 11:52:28 +0800

    修改资源配置.

--- run-localhandler.sh
+++ run-localhandler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mainClass="pers.yzq.spark.datasets.LocalHandler"
+mainClass="pers.yzq.spark.LocalHandler"
 java -Xmx8G -cp /opt/service/spark/spark-app/target/spark-2.0.jar ${mainClass} -Djava.ext.dirs=/opt/service/spark/spark-app/lib
