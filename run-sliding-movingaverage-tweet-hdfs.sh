e63b7ebb3627b9c8388a43001083a1579a2ffa84
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-30 14:20:19 +0800

    修改资源配置.

--- run-sliding-movingaverage-tweet-hdfs.sh
+++ run-sliding-movingaverage-tweet-hdfs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-libs_dir="hdfs://node6:9000/libs"
+libs_dir="hdfs://node1:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 
 spark-submit \
---master spark://node6:7079 \
+--master spark://node1:7079 \
 --executor-memory 32g \
 --executor-cores 16 \
 --driver-cores 16 \
