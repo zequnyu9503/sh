1026da72a856187d441c2d4a1596e35fed5d62b6
zequnyu9503 <greatyuzequn@outlook.com>
2020-12-30 14:07:23 +0800

    修改资源配置.

--- run-tweetbyday.sh
+++ run-tweetbyday.sh
@@ -5,10 +5,10 @@ target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 
 spark-submit \
 --master spark://node1:7079 \
---executor-memory 4g \
---executor-cores 8 \
---driver-cores 8 \
---driver-memory 2g \
+--executor-memory 32g \
+--executor-cores 16 \
+--driver-cores 16 \
+--driver-memory 8g \
 --class pers.yzq.sliding.dataset.TweetByDay \
 --driver-java-options "-Dlog4j.configuration=file:${log_path}" \
 --jars \
