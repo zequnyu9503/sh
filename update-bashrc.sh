22ac8865e00828a7f61ae5f733326b4bc11647d0
zequnyu9503 <greatyuzequn@outlook.com>
2021-01-07 17:04:22 +0800

    hbase

--- update-bashrc.sh
+++ update-bashrc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 slaves=("node1" "node7" "node8" "node9" "node10" "node11")
-param=$0
+param="node7_11"
 
 for slave in ${slaves[@]}
  do
-  scp ././bashrc/node7_15 root@${slave}:/root/.bashrc
+  scp ././bashrc/${param} root@${slave}:/root/.bashrc
   ssh root@${slave} source /root/.bashrc
  done
