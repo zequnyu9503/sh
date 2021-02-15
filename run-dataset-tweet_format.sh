37765dcc5e683b5a8dc7b3b2912274fb191913b0
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-16 00:03:22 +0800

    dataset-tweet_format

--- run-dataset-tweet_format.sh
+++ run-dataset-tweet_format.sh
@@ -4,7 +4,7 @@ log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 
 # 进入工作目录
-# cd "/opt/zequnyu/tweet/"
+cd "/opt/zequnyu/tweet/"
 days=$(seq -f "%02g" 2 2)
 for day in ${days[@]}
 do
@@ -23,5 +23,6 @@ ${target}
 
 hdfs dfs -getmerge "/real-world/precise/2019-04-${day}.json" "2019-04-${day}.json"
 hdfs dfs -rm -f "/real-world/precise/2019-04-${day}.json"
-hdfs dfs -put "2019-04-${day}.json" "/real-world/precise/"
+# hdfs dfs -put "2019-04-${day}.json" "/real-world/precise/"
+# rm -rf "2019-04-${day}.json"
 done
