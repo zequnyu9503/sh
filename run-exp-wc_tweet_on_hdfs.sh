a4ee4e903f466f47a5274aa15300022741ac626f
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-18 10:12:30 +0800

    wc_tweet_on_hdfs

--- run-exp-wc_tweet_on_hdfs.sh
+++ run-exp-wc_tweet_on_hdfs.sh
@@ -3,7 +3,9 @@ libs_dir="hdfs://node1:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 
-rm -f /opt/zequnyu/sh/1:2-3-np.data
+rm -f /opt/zequnyu/sh/wc_tweet_hdfs_1-2_3_p.log
+rm -f /opt/zequnyu/sh/sliding.log
+arm -f /opt/zequnyu/sh/app.log
 spark-submit \
 --master spark://node1:7079 \
 --executor-memory 32g \
