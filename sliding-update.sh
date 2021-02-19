b7688b872feb7e8d8307a3273ebd4abeda5d1452
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-19 19:42:45 +0800

    wc_tweet_on_hdfs

--- sliding-update.sh
+++ sliding-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 path="/opt/service/spark/slidingwindow"
 # slaves=("node1 node7 node8 node9 node10 node11 node12 node13 node14 node15")
- slaves=("node1 node7 node8 node9 node3 node11")
+ slaves=("node1 node6 node7 node8 node9 node11")
 
 echo "只更新Core, Assembly"
 
