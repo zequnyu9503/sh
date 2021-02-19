668464760dc212f53cff3590e79e2b8d921e2e76
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-19 19:58:17 +0800

    wc_tweet_on_hdfs

--- trace_format.sh
+++ trace_format.sh
@@ -1,27 +1,46 @@
 #!/usr/bin/env bash
-cd /opt/zequnyu/alibaba/
+cd /opt/zequnyu/ali/
 # 1:8
+cat cuaa >> 1.csv
+rm -f cuaa
+cat cuab >> 1.csv
+rm -f cuab
 # 2:4
 cat cuac >> 2.csv
+rm -f cuac
 # 3:4
 cat cuad >> 3.csv
+rm -f cuad
 # 4:12
 cat cuae >> 4.csv
+rm -f cuae
 cat cuaf >> 4.csv
+rm -f cuaf
 cat cuag >> 4.csv
+rm -f cuag
 # 5:4
 cat cuah >> 5.csv
+rm -f cuah
 # 6:4
 cat cuai >> 6.csv
+rm -f cuai
 # 7:8
 cat cuaj >> 7.csv
+rm -f cuaj
 cat cuak >> 7.csv
+rm -f cuak
 # 8:8
 cat cual >> 8.csv
+rm -f cual
 cat cuam >> 8.csv
+rm -f cuam
 # 9:4
 cat cuan >> 9.csv
+rm -f cuan
 # 10: 12
 cat cuao >> 10.csv
+rm -f cuao
 cat cuap >> 10.csv
+rm -f cuap
 cat cuaq >> 11.csv
+rm -f cuaq
