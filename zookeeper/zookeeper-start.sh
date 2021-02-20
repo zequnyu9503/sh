13f093e262189164b1cf976d3545eb3c9f83d3d3
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-20 11:25:51 +0800

    batches

--- zookeeper/zookeeper-start.sh
+++ zookeeper/zookeeper-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # slaves=("node1" "node6" "node7" "node8" "node9" "node11")
-slaves=("node1")
+slaves=("node1" "node6")
 path="/opt/service/zookeeper-3.5.5/bin/"
 
 for slave in ${slaves[@]}
