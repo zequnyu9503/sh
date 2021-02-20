4d88de2eb693187854e32e0d9235ed5ad804a6d8
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-20 08:35:18 +0800

    batches

--- sliding-sliding.sh
+++ sliding-sliding.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 path="/opt/service/spark/slidingwindow"
 
-cd ${path};git pull;mvn clean install -DskipTests -pl sliding
+cd ${path};git pull;mvn clean install -DskipTests -pl sliding -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 
 echo "更新完毕"
