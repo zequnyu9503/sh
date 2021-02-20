d51363cf246627e4166b4a0dc86c49160b48f842
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-20 11:17:17 +0800

    batches

---
+++ hbase/hbase-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+slaves=("node1" "node6" "node7" "node8" "node9" "node11")
+path="/opt/service/zookeeper-3.5.5/bin/"
+
+for slave in ${slaves[@]}
+do
+ssh root@${slave} "rm -rf /opt/service/hbase-tmp/*"
+done
