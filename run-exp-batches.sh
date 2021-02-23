3567b3a967e5741184be955639f5c29ffdbc22aa
zequnyu9503 <greatyuzequn@outlook.com>
2021-02-23 14:53:01 +0800

    batches

--- run-exp-batches.sh
+++ run-exp-batches.sh
@@ -2,6 +2,13 @@
 libs_dir="hdfs://node1:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
+slaves=("node3 node7 node8 node9 node11")
+
+rm -f /opt/service/spark/slidingwindow/logs/fp*
+for slave in ${slaves[@]}
+ do
+  scp root@node${slave}:/opt/service/spark/slidingwindow/logs/fp.txt /opt/service/spark/slidingwindow/logs/fp_${slave}.txt
+ done
 
 #spark-submit \
 #--master spark://node1:7079 \
