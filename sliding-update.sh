3a1a2face21a33fa1e105c07af2d529d6aba4826
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-02 14:55:01 +0800

    更新配置

--- sliding-update.sh
+++ sliding-update.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 path="/opt/service/spark/slidingwindow"
-# slaves=("node1 node7 node8 node9 node10 node11 node12 node13 node14 node15")
- slaves=("node1 node3 node7 node8 node9 node11")
+slaves=("node5 node6 node7 node8 node9 node10")
 
 echo "只更新Core, Assembly"
 
 for slave in ${slaves[@]}
  do
-  ssh root@${slave} "cd ${path};git reset --hard;git pull;mvn clean install -DskipTests -pl core,assembly" &
+  ssh root@${slave} "cd ${path}; git pull; mvn clean install -DskipTests -pl core,assembly" &
  done
 wait
 echo "更新完毕"
+
+# git reset --hard
