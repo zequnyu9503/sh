6cff4d7c3fda0156fec84434f49e9afc8b3272ea
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-02 15:58:13 +0800

    更新配置

--- spark-shell.sh
+++ spark-shell.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-libs_dir="hdfs://node1:9000/libs"
+libs_dir="hdfs://node5:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 
 spark-shell \
 --master spark://node1:7079 \
---executor-memory 32g \
---executor-cores 32 \
+--executor-memory 16g \
+--executor-cores 16 \
 --driver-cores 16 \
---driver-memory 32g \
+--driver-memory 16g \
 --driver-java-options "-Dlog4j.configuration=file:${log_path}" \
 --jars \
 ${libs_dir}/fastjson-1.2.35.jar \
