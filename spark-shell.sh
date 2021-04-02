c170dc1deda290eb9fb27362bb62cb306e58c2fe
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-02 16:03:55 +0800

    更新配置

--- spark-shell.sh
+++ spark-shell.sh
@@ -3,7 +3,7 @@ libs_dir="hdfs://node5:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 
 spark-shell \
---master spark://node1:7079 \
+--master spark://node5:7079 \
 --executor-memory 16g \
 --executor-cores 16 \
 --driver-cores 16 \
