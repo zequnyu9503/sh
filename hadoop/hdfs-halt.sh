e4de5c5a505e01e647fac6eb024318c16414f0be
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-02 12:43:19 +0800

    更新配置

--- hadoop/hdfs-halt.sh
+++ hadoop/hdfs-halt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 path="/opt/service/hadoop-2.10.1/sbin/"
 
-echo "主节点是node1"
+echo "主节点是node5"
 sh "${path}stop-dfs.sh"
--- hadoop/hdfs-start.sh
+++ hadoop/hdfs-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 path="/opt/service/hadoop-2.10.1/sbin/"
 
-echo "主节点是node1"
-echo "从节点是node3,node7,node8,node9,node11"
+echo "主节点是node5"
+echo "从节点是node6,node7,node8,node9,node10"
 
 sh "${path}start-dfs.sh"
