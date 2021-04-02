8e5d1633f195783f1d81a6919502c03db1850ce4
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-02 16:00:18 +0800

    更新配置

--- sliding-fresh.sh
+++ sliding-fresh.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 path="/opt/service/spark/slidingwindow"
-# slaves=("node1 node7 node8 node9 node10 node11 node12 node13 node14 node15")
- slaves=("node1 node6 node7 node8 node9 node11")
-
-echo "只更新编译"
+slaves=("node5 node6 node7 node8 node9 node10")
 
 for slave in ${slaves[@]}
  do
-  ssh root@${slave} "cd ${path};git reset --hard;git pull" &
+  ssh root@${slave} "cd ${path}; git pull" &
  done
 wait
 echo "更新完毕"
