5ca1dfe2cee0813988c93d5bd8158153f39dc1ee
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-03 13:21:43 +0800

    更新配置

--- dataset/bulkload_tweet.sh
+++ dataset/bulkload_tweet.sh
@@ -3,4 +3,5 @@ mainClass="pers.yzq.sliding.dataset.BulkLoad_Tweet"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 lib="/opt/service/spark/lib"
 
-scala -classpath ${target} ${mainClass} -Djava.ext.dirs=${lib}
+scala -classpath ${target} ${mainClass}
+# -Djava.ext.dirs=${lib}
