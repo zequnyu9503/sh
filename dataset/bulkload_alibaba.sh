3a67aa857951bb1454f261717b59945fc47a7b21
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-06 22:35:31 +0800

    更新配置

--- dataset/bulkload_alibaba.sh
+++ dataset/bulkload_alibaba.sh
@@ -11,4 +11,19 @@ spark-submit \
 --driver-memory 16g \
 --class pers.yzq.sliding.dataset.BulkLoad_Alibaba \
 --driver-java-options "-Dlog4j.configuration=file:${log_path}" \
+--jars \
+${libs_dir}/fastjson-1.2.35.jar,\
+${libs_dir}/hbase-common-2.1.4.jar,\
+${libs_dir}/hadoop-common-2.7.7.jar,\
+${libs_dir}/hbase-client-2.1.4.jar,\
+${libs_dir}/hbase-shaded-miscellaneous-2.1.0.jar,\
+${libs_dir}/hbase-protocol-shaded-2.1.4.jar,\
+${libs_dir}/hbase-shaded-protobuf-2.1.0.jar,\
+${libs_dir}/hbase-shaded-netty-2.1.0.jar,\
+${libs_dir}/hbase-protocol-2.1.4.jar,\
+${libs_dir}/htrace-core4-4.2.0-incubating.jar,\
+${libs_dir}/hbase-mapreduce-2.1.4.jar,\
+${libs_dir}/hbase-server-2.1.4.jar,\
+${libs_dir}/hbase-shaded-client-2.1.4.jar,\
+${libs_dir}/hbase-zookeeper-2.1.4.jar \
 ${target}
