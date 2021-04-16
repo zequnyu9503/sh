571fe52a39960dbf77afa8777688eaac064ca1bb
zequnyu9503 <greatyuzequn@outlook.com>
2021-04-16 14:41:31 +0800

    更新配置

--- dataset/twitter_analysis.sh
+++ dataset/twitter_analysis.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-libs_dir="hdfs://node1:9000/libs"
+libs_dir="hdfs://node5:9000/libs"
 log_path="/opt/service/spark/slidingwindow/conf/log4j.properties"
 target="/opt/service/spark/slidingwindow/sliding/target/sliding-2.4.4.jar"
 
 spark-submit \
---master spark://node1:7079 \
+--master spark://node5:7079 \
 --executor-memory 16g \
 --executor-cores 16 \
 --driver-cores 16 \
